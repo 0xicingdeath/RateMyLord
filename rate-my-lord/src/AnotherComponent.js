8f26f4d37341a20cb18b08a6bc3ae6224463fc1b
Charlie Cheever <ccheever@gmail.com>
2017-09-23 02:34:53 -0400

    Adding AnotherComponent; fixing package.json missing entries

---
+++ rate-my-lord/src/AnotherComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class AnotherComponent extends React.Component {
+    render() {
+        return (
+            <span>This is AnotherComponent</span>
+        )
+    }
+}
